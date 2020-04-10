94b472abd15cc15ecc7a69db00f65c8038dc009e
Becky Beauchamp <bbeauchamp@bird.co>
2020-04-09 19:19:20 -0700

    Updates

---
+++ precache-manifest.9488ce153f60b9c1ffe61b9df65e9819.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20d430a2db9e77884d10b95628e1d505",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "ee4baad708210247f259",
+    "url": "/my-app/static/css/main.77708614.chunk.css"
+  },
+  {
+    "revision": "a85c963c3b7d610b481a",
+    "url": "/my-app/static/js/2.f4f7c78a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app/static/js/2.f4f7c78a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee4baad708210247f259",
+    "url": "/my-app/static/js/main.21910a81.chunk.js"
+  },
+  {
+    "revision": "ead4569107822d54f6dd",
+    "url": "/my-app/static/js/runtime-main.57569933.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
